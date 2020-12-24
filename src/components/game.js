569a18f79551ac73dc0e0ef2a09d48ab049316bc
raulsposito <raulsposito@gmail.com>
2020-12-23 23:24:36 -0300

    refactored game.js

--- src/components/game.js
+++ src/components/game.js
@@ -36,30 +36,3 @@ function resumeGame() {
     // postScore();
     // let score = document.querySelector("#score");
 }
-
-function getArrowsInput() {
-    let up = document.getElementById("button-up")
-    let left = document.getElementById("button-left")
-    let right = document.getElementById("button-right")
-    let down = document.getElementById("button-down")
-
-    document.addEventListener("keydown", (e) => {
-
-        switch (e.code) {
-            case 38:
-                left;
-                break;
-            case 38: 
-                up;
-                break;
-            case 39:
-                right;
-                break;
-            case 40:
-                down;
-                break;
-        }
-    })
-}
-
-
