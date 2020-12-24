1e002833909b85d1c1a7c15732940b015437541d
raulsposito <raulsposito@gmail.com>
2020-12-23 23:55:23 -0300

    add keys to stage

--- src/services/handleKeys.js
+++ src/services/handleKeys.js
@@ -1,23 +1,30 @@
-function getArrowsInput() {
-    let up = document.getElementById("button-up")
-    let left = document.getElementById("button-left")
-    let right = document.getElementById("button-right")
-    let down = document.getElementById("button-down")
+// function getArrowsInput() {
+//     let up = document.getElementById("button-up")
+//     let left = document.getElementById("button-left")
+//     let right = document.getElementById("button-right")
+//     let down = document.getElementById("button-down")
+// 
+//     document.addEventListener("keydown", (e) => {
+//         switch (e.code) {
+//             case 38:
+//                 left();
+//                 break;
+//             case 38: 
+//                 up();
+//                 break;
+//             case 39:
+//                 right();
+//                 break;
+//             case 40:
+//                 down();
+//                 break;
+//         }
+//     })
+// }
 
-    document.addEventListener("keydown", (e) => {
-        switch (e.code) {
-            case 38:
-                left();
-                break;
-            case 38: 
-                up();
-                break;
-            case 39:
-                right();
-                break;
-            case 40:
-                down();
-                break;
-        }
-    })
-}
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+        let left = document.getElementById("button-left")
+        left.style.display = 'none';
+    }
+})
